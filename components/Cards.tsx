a3f33a04d80ecceb64308a126697d3474caa6ef4
Arno <arno.k.johny@gmail.com>
2023-08-14 14:53:05 +0200

    fixed responsiveness for md screens, and worked on text details

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -9,20 +9,19 @@ export interface CardsProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Cards = ({ headline, subline }: CardsProps) => {
   return (
-    <div className="flex flex-col  space-y-24 mt-44 ">
+    //Wenn man von 1024px runter scaled gehen die Karten kurz vor md aus dem Bild
+    <div className="flex flex-col space-y-24 mt-44">
       <div className="flex flex-col gap-4">
         <Heading>{headline}</Heading>
         <div className="">
           <Heading variant={HEADING_VARIANTS.H2}>{subline}</Heading>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row gap-10">
+      <div className="flex flex-col self-center lg:flex-row gap-10">
         <Card
           cardName={"Design"}
           cardTag={"create"}
-          text={
-            "Lorem ipsum dolor sit amet sijfds ijsdbfdsfj isdjbfjfb sjdbjs us sudhsu udbdu bsusbdjs usbd su busbd usbd sbjbd ds sdjbsdj sjbd jsb sdjb"
-          }
+          text={"Lorem ipsum dolor sit amet"}
           imagePath={"/assets/CardDesign.svg"}
         />
 
--- components/base/Card.tsx
+++ components/base/Card.tsx
@@ -10,18 +10,20 @@ export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Card = ({ cardName, text, cardTag, imagePath }: CardProps) => {
   return (
-    <div className="w-[18rem] h-[25rem] border-purple shadow-card rounded-lg">
+    <div className="w-[18rem] h-[25rem] lg:w-auto lg:h-[28rem] border-purple shadow-card rounded-lg">
       <div className="flex max-w-full justify-between pt-10 px-10">
         <Image src={imagePath} width={60} height={60} alt="" />
 
         <div className="min-w-[6.25rem] min-h-[2rem] flex justify-center w-fit h-fit self-center bg-white rounded-[37px]">
-          <label className="self-center block text-black ">{cardTag}</label>
+          <label className="self-center block text-black leading-3 tracking-widest">
+            {cardTag}
+          </label>
         </div>
       </div>
-      <label className="text-white flex mt-11 ml-10 text-xl tracking-[0.15em]">
+      <label className="text-white ml-10 flex mt-11 text-xl tracking-[0.15em]">
         {cardName}
       </label>
-      <p className="text-white flex text-sm ml-11 mr-5 font-light tracking-widest leading-8">
+      <p className="text-white flex text-sm mx-10  font-light tracking-widest leading-8">
         {text}
       </p>
     </div>
